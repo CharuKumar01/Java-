a3a4b4a4988df5c01ef1366920f5af7fb4fcec79
CharuKumar01 <kumarcharu83@gmail.com>
2024-07-26 15:22:16 +0530

    java

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // System.out.println("Hello");
+
+        // System.out.println(2>3);
+        int num1 = 1;
+        int num2 = 2;
+        int totalNum;
+        totalNum = num1 + num2;
+        System.err.println(totalNum);
     }
 }
