bb82bf00b9c97d0f499fe01d7c962728e5946051
CharuKumar01 <kumarcharu83@gmail.com>
2024-07-29 00:56:55 +0530

    java

--- FirstClass.java
+++ FirstClass.java
@@ -6,7 +6,9 @@ public class FirstClass {
         int num1 = 1;
         int num2 = 2;
         int totalNum;
-        totalNum = num1 + num2;
+        totalNum = num1 + num2; 
+        int myArray[] = {1,2,3,4,5,6};
+        System.out.println(myArray);
         System.err.println(totalNum);
     }
 }
